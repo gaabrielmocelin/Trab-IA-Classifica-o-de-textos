e6e59bccda7660b4cd52d77e3ce4f8010a536ed8
Gabriel Mocelin <gabriel.mocelin@acad.pucrs.br>
2017-06-29 20:01:47 -0300

    initial commit

---
+++ Classificacao de textos/Classificacao de textos/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classificacao de textos
+//
+//  Created by Gabriel Mocelin on 29/06/17.
+//  Copyright © 2017 Gabriel Mocelin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
